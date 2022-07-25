5019da105ce768daf08a43cc0aac1150f0177c63
Nivethakamal <nivethakamal99@gmail.com>
2022-07-25 10:25:35 +0530

    Diamond Problem

---
+++ Practice Project/Assisted Practice/Lesson3/Diamond Problem/Diamond.java
@@ -0,0 +1,17 @@
+package Lesson3DiamondProb;
+
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println(" First "); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println(" Second"); 
+    } 
+}  
+
---
+++ Practice Project/Assisted Practice/Lesson3/Diamond Problem/TestClass.java
@@ -0,0 +1,15 @@
+package Lesson3DiamondProb;
+
+public class TestClass implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        TestClass ob = new TestClass(); 
+        ob.show(); 
+    } 
+}
