3034c02fd0484fd7fb7f1eb1e065f80478501529
Nivethakamal <nivethakamal99@gmail.com>
2022-07-27 20:48:47 +0530

    Linear Search

---
+++ Assignments/Assisted Practice/Lesson5/LinearSearch.java
@@ -0,0 +1,40 @@
+package Lesson5;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+	
+public static void main(String[] args) {
+        
+        int  [] arr= {119,234,60,35,10};
+        
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Element to  be Searced");
+        
+        int SearchValue= sc.nextInt();
+        
+        int result= linearing(arr,SearchValue);
+        
+        if(result==-1) {
+            System.out.println("Element Not Found In The Array");
+        }
+        else {
+            System.out.println("Element Found at index: ["+result+"] , and Search  key is :"+arr[result]);
+        }
+        
+    }
+    
+    private static int linearing(int[] arr,int searchValue) {
+        
+                 
+        for(int i=0; i<arr.length; i++) {
+            
+            if(arr[i]==searchValue) {
+                return i;
+            }
+            
+        }
+        return -1;
+    }
+
+}
