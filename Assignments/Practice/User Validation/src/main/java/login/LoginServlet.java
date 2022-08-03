517b1c63ac39ccf0610362266a14e8f708f7ae14
Nivethakamal <nivethakamal99@gmail.com>
2022-08-03 10:15:11 +0530

    User Validation

---
+++ Assignments/Practice/User Validation/src/main/java/login/LoginServlet.java
@@ -0,0 +1,46 @@
+package login;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		 
+	
+			String email =req.getParameter("userid");
+			        String password =req.getParameter("pass");
+			        
+			        
+			        if(email.equalsIgnoreCase("nivethakamal99@gmail.com") && password.equalsIgnoreCase("nivesha")) {
+			            //add  user in the session once the login is done
+			            HttpSession session=req.getSession();
+			            session.setAttribute("email", email);
+			            session.setAttribute("pass", password);
+			            resp.sendRedirect("dashboard");
+			        }
+			        else {
+			            resp.sendRedirect("index.html");
+			        }
+		
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+
+}
