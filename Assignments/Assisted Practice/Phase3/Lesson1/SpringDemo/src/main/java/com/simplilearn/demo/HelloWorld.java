780355bca65bc48a57f382a23a56051b8e44253c
Nivethakamal <nivethakamal99@gmail.com>
2022-08-19 11:30:08 +0530

    Lesson 1 Spring,JDBC Assisted

---
+++ Assignments/Assisted Practice/Phase3/Lesson1/SpringDemo/src/main/java/com/simplilearn/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.simplilearn.demo;
+
+
+public class HelloWorld {
+	
+	public void print() {
+		System.out.println("Welcome to Spring Framework");
+	}
+
+}
---
+++ Assignments/Assisted Practice/Phase3/Lesson1/SpringJDBC/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>List Of Products</h2>
+<a href="listproduct">List All Products</a>
+</body>
+</html>
